069327196fcc63810118a3b8bab612e770b7cba3
guxiaoyan <651741347@qq.com>
2022-04-06 17:20:54 +0800

    添加文件

--- Hello/src/main/java/com/gxy/maven/Hello.java
+++ Hello/src/main/java/com/gxy/maven/Hello.java
@@ -4,7 +4,11 @@ package com.gxy.maven;
  * Created by guxiaoyan on 2022/4/5.
  */
 public class Hello {
+
     public String sayHello(String name) {
+        System.out.println("sad");
         return "Hello " + name + "!";
     }
+
+
 }
---
+++ Hello/src/main/java/com/gxy/maven/Word.java
@@ -0,0 +1,15 @@
+package com.gxy.maven;
+
+/**
+ * @author guxiaoyan
+ * @create 2022-04-06 13:29
+ */
+public class Word {
+
+    public static void main(String[] args) {
+        System.out.println("gxy");
+    }
+
+
+
+}
