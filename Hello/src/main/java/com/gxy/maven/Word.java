7a8cf7d32232e87fe65900eab4556990549e39b2
guxiaoyan <651741347@qq.com>
2022-04-06 21:54:37 +0800

    创建分支

--- Hello/src/main/java/com/gxy/maven/Word.java
+++ Hello/src/main/java/com/gxy/maven/Word.java
@@ -9,6 +9,7 @@ public class Word {
     public static void main(String[] args) {
         System.out.println("gxy");
         System.out.println("更新1");
+        System.out.println("创建分支");
     }
 
 
